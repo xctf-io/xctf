3305202860393b7d086c6675129944a4eee8bf2c
Alex Vanderpot <alex@vanderpot.com>
2021-07-02 22:02:11 -0700

    lint

--- frontend/src/TestForm.tsx
+++ frontend/src/TestForm.tsx
@@ -4,24 +4,25 @@ import { useEffect } from "react";
 import { GQLHooks } from "./generated/hasura/react";
 
 gql`
-    fragment TeamAsCaptain on teams {
-        affiliation
-        bracket
-        captain {
-            id
-        }
-        country
-        email
-        name
-        website
+  fragment TeamAsCaptain on teams {
+    affiliation
+    bracket
+    captain {
+      id
     }
+    country
+    email
+    name
+    website
+  }
 `;
 
 export function MyTeam() {
   const authContext = useAuthContext();
-  const [loadTeamForm, { data }] = GQLHooks.Fragments.TeamAsCaptain.useQueryByIdLazy(
-    { teamsId: authContext.current_user?.captain_of?.id || 0 }
-  );
+  const [loadTeamForm, { data }] =
+    GQLHooks.Fragments.TeamAsCaptain.useQueryByIdLazy({
+      teamsId: authContext.current_user?.captain_of?.id || 0,
+    });
 
   useEffect(() => {
     if (authContext?.current_user?.captain_of?.id) loadTeamForm({});
