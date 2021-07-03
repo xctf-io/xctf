975d1e9fdba23dc5bcf2c441222cf8254fc30e30
Alex Vanderpot <alex@vanderpot.com>
2021-07-02 22:01:04 -0700

    add schema

--- frontend/src/TestForm.tsx
+++ frontend/src/TestForm.tsx
@@ -1,67 +1,31 @@
 import { gql } from "@apollo/client";
-import { useTeamCaptainFormLazyQuery } from "generated/graphql";
 import { useAuthContext } from "context/AuthContext";
 import { useEffect } from "react";
+import { GQLHooks } from "./generated/hasura/react";
 
 gql`
-  query TeamCaptainForm($id: Int = 0) {
-    teams_by_pk(id: $id) {
-      affiliation
-      bracket
-      captain {
-        id
-      }
-      country
-      email
-      name
-      website
+    fragment TeamAsCaptain on teams {
+        affiliation
+        bracket
+        captain {
+            id
+        }
+        country
+        email
+        name
+        website
     }
-  }
 `;
 
-/*
-const schemaExtras = {
-  id: {
-    allowedValues: [1, 2, 3],
-  },
-  title: {
-    options: [
-      { label: 1, value: "a" },
-      { label: 2, value: "b" },
-    ],
-  },
-  "author.firstName": {
-    placeholder: "John",
-  },
-};
-
-const schemaValidator = (model: object) => {
-  const details = [];
-
-
-  // ...
-
-  return details.length ? { details } : null;
-};
-
-const bridge = new GraphQLBridge(
-  TeamCaptainFormDocument.getType('teams'),
-  schemaValidator,
-  schemaExtras,
-);
-
- */
-
-export function TestForm() {
+export function MyTeam() {
   const authContext = useAuthContext();
-  const [loadTeamForm, { data, loading, error, called }] =
-    useTeamCaptainFormLazyQuery({
-      variables: {
-        id: authContext?.current_user?.id,
-      },
-    });
+  const [loadTeamForm, { data }] = GQLHooks.Fragments.TeamAsCaptain.useQueryByIdLazy(
+    { teamsId: authContext.current_user?.captain_of?.id || 0 }
+  );
+
   useEffect(() => {
-    if (authContext?.current_user?.captain_of?.id) loadTeamForm();
+    if (authContext?.current_user?.captain_of?.id) loadTeamForm({});
   }, [authContext, loadTeamForm]);
+
   return <p>{JSON.stringify(data)}</p>;
 }
--- frontend/src/customFragments.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const TeamFragmentDoc =gql`
-    fragment Team on teams {
-        id
-
-    }
-
-`
