363d5fa5357a54641ffc8d9207889f8650d9e7b5
Alex Vanderpot <alex@vanderpot.com>
2021-07-03 23:17:34 -0700

    permissions

--- hasura/migrations/ctfd/1625378134188_score_timeline_view/up.sql
+++ hasura/migrations/ctfd/1625378134188_score_timeline_view/up.sql
@@ -8,14 +8,16 @@ select team_id, user_id, awards.date, awards.value
 from public.awards;
 
 CREATE VIEW public.score_timeline_user AS
-SELECT score_events.team_id,
+SELECT score_events.event_time,
+       score_events.team_id,
        score_events.user_id,
        sum(score_events.event_value)
        over (partition by team_id, user_id order by score_events.event_time rows between unbounded preceding and current row) as total
 FROM score_events;
 
 CREATE VIEW public.score_timeline AS
-SELECT score_events.team_id,
+SELECT score_events.event_time,
+       score_events.team_id,
        sum(score_events.event_value)
        over (partition by team_id order by score_events.event_time rows between unbounded preceding and current row) as total
 FROM score_events;
