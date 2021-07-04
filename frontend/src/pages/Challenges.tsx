a0bbcd680e48a628ce3f59e8c9393717d3ed3e6f
Alex Vanderpot <alex@vanderpot.com>
2021-07-03 18:43:00 -0700

    challenges

--- frontend/src/pages/Challenges.tsx
+++ frontend/src/pages/Challenges.tsx
@@ -1,5 +1,12 @@
 import { gql } from "@apollo/client";
 import { GQLHooks } from "../generated/hasura/react";
+import { Card, StyledBody, StyledAction } from "baseui/card";
+import { Button } from "baseui/button";
+import { Tag, KIND, VARIANT } from "baseui/tag";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrophy } from "@fortawesome/free-solid-svg-icons";
+import { Icon } from "baseui/icon";
+import { Grid } from "baseui/layout-grid";
 
 gql`
   fragment ChallengeList on challenges {
@@ -35,5 +42,34 @@ export default function Challenges() {
   const { data } = GQLHooks.Fragments.ChallengeList.useQueryObjects({
     variables: { limit: 10 },
   });
-  return <p>{JSON.stringify(data)}</p>;
+  return (
+    <Grid>
+      {data?.challenges.map((chal) => (
+        <Card
+          title={
+            <>
+              {chal.name}{" "}
+              <Tag
+                closeable={false}
+                kind={KIND.positive}
+                variant={VARIANT.solid}
+              >
+                <Icon>
+                  <FontAwesomeIcon icon={faTrophy} />
+                </Icon>
+                {chal.value}
+              </Tag>
+            </>
+          }
+        >
+          <StyledBody></StyledBody>
+          <StyledAction>
+            <Button overrides={{ BaseButton: { style: { width: "100%" } } }}>
+              Button Label
+            </Button>
+          </StyledAction>
+        </Card>
+      ))}
+    </Grid>
+  );
 }
