390bd76f64931bf31590be7014eaea8031562528
Alex Vanderpot <alex@vanderpot.com>
2021-07-03 17:59:14 -0700

    challenge list query

--- frontend/src/pages/Challenges.tsx
+++ frontend/src/pages/Challenges.tsx
@@ -1,3 +1,39 @@
+import { gql } from "@apollo/client";
+import { GQLHooks } from "../generated/hasura/react";
+
+gql`
+  fragment ChallengeList on challenges {
+    id
+    name
+    category
+    description
+    value
+    max_attempts
+    files {
+      id
+      location
+      type
+    }
+    hints {
+      id
+      type
+      cost
+      unlocked_content {
+        id
+        content
+      }
+    }
+    tags {
+      id
+      value
+    }
+    type
+  }
+`;
+
 export default function Challenges() {
-  return <p>challenges</p>;
+  const { data } = GQLHooks.Fragments.ChallengeList.useQueryObjects({
+    variables: { limit: 10 },
+  });
+  return <p>{JSON.stringify(data)}</p>;
 }
