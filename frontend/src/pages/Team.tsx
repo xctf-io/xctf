4e27ae6d192d5147a581e79efda6e1676e1ae74b
Alex Vanderpot <alex@vanderpot.com>
2021-07-05 15:13:47 -0700

    everything

--- frontend/src/pages/Team.tsx
+++ frontend/src/pages/Team.tsx
@@ -1,35 +1,38 @@
 import { gql } from "@apollo/client";
 import { GQLHooks } from "../generated/hasura/react";
 import { useParams } from "react-router-dom";
+import { Display2, Display4, H1, H5 } from "baseui/typography";
+import { Avatar } from "baseui/avatar";
+import { Block } from "baseui/block";
 
 gql`
-  fragment SingleTeam on teams {
-    id
-    name
-    affiliation
-    bracket
-    country
-    created
-    website
-    members {
-      id
-      name
-      affiliation
-      avatar
-      bracket
-      country
-      type
-      website
-      captain_of {
+    fragment SingleTeam on teams {
         id
-      }
-      score
+        name
+        affiliation
+        bracket
+        country
+        created
+        website
+        members {
+            id
+            name
+            affiliation
+            avatar
+            bracket
+            country
+            type
+            website
+            captain_of {
+                id
+            }
+            score
+        }
+        score_timeline {
+            event_time
+            score
+        }
     }
-    score_timeline {
-      event_time
-      score
-    }
-  }
 `;
 
 type TeamParams = {
@@ -41,5 +44,30 @@ export default function Team() {
   const { data } = GQLHooks.Fragments.SingleTeam.useQueryById({
     teamsId: parseInt(id),
   });
-  return <p>{JSON.stringify(data)}</p>;
+  const t = data?.teams_by_pk;
+
+  return (<>
+    <Display2>{t?.name}</Display2>
+    <Display4>nth Place</Display4>
+    <Display4>{t?.score_timeline?.slice(-1)[0]?.score} Points</Display4>
+    <H1>Members</H1>
+    <Block
+      display={"flex"}>
+
+
+      {t?.members.map((u) => (
+        <Block
+        paddingRight={'scale600'}
+        >
+          <Avatar
+            size="scale3200"
+            name={u?.name || ""}
+            src={u?.avatar || undefined}
+          />
+          <H5 marginTop={'scale600'}>{u?.name}</H5>
+        </Block>
+      ))}
+    </Block>
+    {JSON.stringify(data)}
+  </>);
 }
