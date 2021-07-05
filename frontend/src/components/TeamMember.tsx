16a07dda7ef8cca3161d4bb56193feb46cd30f18
Alex Vanderpot <alex@vanderpot.com>
2021-07-05 16:12:18 -0700

    score overlay

--- frontend/src/components/TeamMember.tsx
+++ frontend/src/components/TeamMember.tsx
@@ -2,11 +2,11 @@ import { SingleTeamFragment } from "../generated";
 import { useStyletron } from "baseui";
 import { Block } from "baseui/block";
 import { Avatar } from "baseui/avatar";
-import { Label1 } from "baseui/typography";
 import { KIND, Tag, VARIANT } from "baseui/tag";
 import { Icon } from "baseui/icon";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrophy } from "@fortawesome/free-solid-svg-icons";
+import { Label1 } from "baseui/typography";
+import { faTrophy } from "@fortawesome/free-solid-svg-icons/faTrophy";
 
 type TeamMemberProps = {
   user: SingleTeamFragment["members"][0];
@@ -23,22 +23,41 @@ export function TeamMember({ user }: TeamMemberProps) {
         alignItems: "center",
       })}
     >
-      <Avatar
-        size="scale2400"
-        name={user?.name || ""}
-        src={user?.avatar || undefined}
-      />
+      <Block position={"relative"}>
+        <Avatar
+          size="scale2400"
+          name={user?.name || ""}
+          src={user?.avatar || undefined}
+        />
+        {user?.score ? (
+          <Tag
+            closeable={false}
+            kind={KIND.positive}
+            variant={VARIANT.solid}
+            overrides={{
+              Root: {
+                style: {
+                  position: "absolute",
+                  bottom: 0,
+                  right: 0,
+                  zIndex: 1,
+                  marginRight: -10,
+                },
+              },
+            }}
+          >
+            <Icon>
+              <FontAwesomeIcon icon={faTrophy} />
+            </Icon>{" "}
+            {user?.score}
+          </Tag>
+        ) : null}
+      </Block>
+
       <Label1>{user?.name} </Label1>
       {/* todo flag icon */}
       {user?.country ? user?.country : null}
-      {user?.score ? (
-        <Tag closeable={false} kind={KIND.positive} variant={VARIANT.solid}>
-          <Icon>
-            <FontAwesomeIcon icon={faTrophy} />
-          </Icon>{" "}
-          {user?.score}
-        </Tag>
-      ) : null}
+
       {user?.captain_of?.id ? (
         <Tag closeable={false} kind={KIND.neutral}>
           Captain
