141dde917406d8917f01f3d5803c10ce47e1b01e
breadchris <chris@lunasec.io>
2023-03-13 17:55:22 -0700

    update home

--- client/src/routes/Home.svelte
+++ client/src/routes/Home.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
-  import { Heading, P } from 'flowbite-svelte';
+  import { Button, Heading, P } from 'flowbite-svelte';
 </script>
 
 <div class="text-center">
-  <Heading tag="h3">Welcome to CTFg!</Heading>
-  <P class="text-center">Register/Login to get started.</P>
+  <div class="mb-4">
+    <Heading tag="h3">Welcome to CTFg!</Heading>
+    <P class="text-center">Register/Login to get started.</P>
+  </div>
+  <hr />
+  <div class="mt-4">
+    <P class="text-center">For help related to the competition, go to the wiki.</P>
+    <Button color="blue" href="https://wiki.ctfg.io">wiki</Button>
+  </div>
 </div>
