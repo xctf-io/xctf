412d70aed272e3628c6d5302885628ae2cce65f8
breadchris <chris@lunasec.io>
2023-03-13 20:27:02 -0700

    update

--- cmd/manage/main.go
+++ cmd/manage/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"context"
-	"log"
+	"github.com/rs/zerolog/log"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
 
 	"github.com/ctfg/ctfg/gen/ctfg"
 	"github.com/twitchtv/twirp"
@@ -25,7 +29,7 @@ func cliLogin(cliCtx *cli.Context) (ctx context.Context, err error) {
 
 	backend := ctfg.NewBackendJSONClient(url, http.DefaultClient, twirp.WithClientPathPrefix("/twirp/backend"), twirp.WithClientHooks(&hooks))
 
-	resp, err := backend.Login(cliCtx.Context, &ctfg.LoginRequest{
+	_, err = backend.Login(cliCtx.Context, &ctfg.LoginRequest{
 		Email:    email,
 		Password: password,
 	})
@@ -34,11 +38,44 @@ func cliLogin(cliCtx *cli.Context) (ctx context.Context, err error) {
 	}
 
 	headers := http.Header{
-		"session": []string{resp.Session},
+		//"session": []string{resp.Session},
 	}
 
 	ctx, err = twirp.WithHTTPRequestHeaders(cliCtx.Context, headers)
 	return
+}
+
+type Challenge struct {
+	Name string `yaml:"name"`
+	Flag string `yaml:"flag"`
+}
+
+func crawlDir(dir string, cb func(chal Challenge) error) error {
+	return filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if info.Name() == "chal.yaml" {
+			data, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			var challenge []Challenge
+			if err := yaml.Unmarshal(data, &challenge); err != nil {
+				return err
+			}
+			chal := challenge[0]
+			log.Info().Str("name", chal.Name).Str("flag", chal.Flag).Msg("found challenge")
+			if err = cb(chal); err != nil {
+				log.Error().Err(err).Msg("failed to submit challenge")
+				return err
+			}
+		}
+		return nil
+	})
 }
 
 func main() {
@@ -62,6 +99,30 @@ func main() {
 			{
 				Name: "flags",
 				Subcommands: []*cli.Command{
+					{
+						Name: "sync",
+						Action: func(ctx *cli.Context) error {
+							url := ctx.String("url")
+							dir := ctx.Args().First()
+
+							// walk dir looking for files named "chal.yaml"
+							// for each file, read the yaml and upsert the challenge
+							cb := func(chal Challenge) error {
+								if chal.Flag == "" {
+									log.Warn().Str("name", chal.Name).Msg("skipping challenge with empty flag")
+									return nil
+								}
+
+								client := ctfg.NewAdminJSONClient(url, http.DefaultClient, twirp.WithClientPathPrefix("/twirp/admin"))
+								_, err := client.UpsertChallenge(ctx.Context, &ctfg.UpsertChallengeRequest{
+									ChallengeName: chal.Name,
+									Flag:          chal.Flag,
+								})
+								return err
+							}
+							return crawlDir(dir, cb)
+						},
+					},
 					{
 						Name: "add",
 						Flags: []cli.Flag{
@@ -113,6 +174,6 @@ func main() {
 	}
 
 	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
+		log.Error().Err(err).Msg("error running app")
 	}
 }
