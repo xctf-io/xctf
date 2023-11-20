3361903d95b3d41f2ae87a90284ebd53e828860d
just-luk <lukegriffith@gmail.com>
2023-11-20 15:52:51 -0500

    Fix navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -6,15 +6,13 @@ import {
 	Link,
 	Navbar,
 	Text,
-	useTheme,
-	Dropdown,
-	Avatar,
-
+	useTheme
 } from "@nextui-org/react";
 import { BsMoonStarsFill, BsSunFill } from "react-icons/bs";
 import { GiFlyingFlag } from "react-icons/gi";
 import { useDarkMode } from "usehooks-ts";
 import {Link as RouterLink, useLocation, useNavigate} from 'react-router-dom';
+import UserDropdown from "./UserDropdown";
 
 interface NavbarProps {
 	links: NavLink[];
@@ -27,7 +25,6 @@ const NavbarComponent = ({ links }: NavbarProps) => {
 
 	const userLoggedIn = !!user;
 	const isAdmin = user?.type === "admin";
-	const themeHexColor = isAdmin ? "DF3562" : "3070ED";
 	const themeColor = isAdmin ? "error" : "primary";
 	const {toggle} = useDarkMode(false);
 	const { type, isDark } = useTheme();
@@ -78,58 +75,7 @@ const NavbarComponent = ({ links }: NavbarProps) => {
 				>
 					{isDark ? <BsSunFill /> : <BsMoonStarsFill />}
 				</Navbar.Link>
-				{user && (
-					<Navbar.Item>
-						<Dropdown placement="bottom-right">
-							<Dropdown.Trigger>
-								<Avatar
-									bordered
-									as="button"
-									color={themeColor}
-									size="md"
-									src={
-										"https://api.dicebear.com/6.x/bottts/svg?baseColor=" +
-										themeHexColor +
-										"&seed=" +
-										user.username
-									}
-								/>
-							</Dropdown.Trigger>
-							<Dropdown.Menu
-								aria-label="User menu actions"
-								onAction={(actionKey) => {
-									if (actionKey === "logout") {
-										logout();
-									}
-								}}
-								containerCss={{ border: "none" }}
-							>
-								<Dropdown.Item
-									key="profile"
-									css={{
-										height: "auto",
-										paddingTop: "0.5rem",
-										paddingBottom: "0.5rem",
-									}}
-								>
-									<Text color="inherit">Signed in as</Text>
-									<Text
-										b
-										color="inherit"
-										style={{
-											overflowWrap: "anywhere",
-										}}
-									>
-										{user.username}
-									</Text>
-								</Dropdown.Item>
-								<Dropdown.Item key="logout" withDivider color="error">
-									Logout
-								</Dropdown.Item>
-							</Dropdown.Menu>
-						</Dropdown>
-					</Navbar.Item>
-				)}
+				{user ? <UserDropdown /> : null}
 			</Navbar.Content>
 			<Navbar.Collapse>
 				{links.map((l) => {
@@ -160,60 +106,7 @@ const NavbarComponent = ({ links }: NavbarProps) => {
 				>
 					{isDark ? <BsSunFill /> : <BsMoonStarsFill />}
 				</Navbar.Link>
-				{user && (
-					<Navbar.Item>
-						<Dropdown placement="bottom-right">
-							<Dropdown.Trigger>
-								<Avatar
-									bordered
-									as="button"
-									color={themeColor}
-									size="md"
-									src={
-										"https://api.dicebear.com/6.x/bottts/svg?baseColor=" +
-										themeHexColor +
-										"&seed=" +
-										user.username
-									}
-								/>
-							</Dropdown.Trigger>
-							<Dropdown.Menu
-								aria-label="User menu actions"
-								onAction={(actionKey) => {
-									if (actionKey === "logout") {
-										logout();
-										document.location.href = "/login";
-										navigate("/login");
-									}
-								}}
-								containerCss={{ border: "none" }}
-							>
-								<Dropdown.Item
-									key="profile"
-									css={{
-										height: "auto",
-										paddingTop: "0.5rem",
-										paddingBottom: "0.5rem",
-									}}
-								>
-									<Text color="inherit">Signed in as</Text>
-									<Text
-										b
-										color="inherit"
-										style={{
-											overflowWrap: "anywhere",
-										}}
-									>
-										{user.username}
-									</Text>
-								</Dropdown.Item>
-								<Dropdown.Item key="logout" withDivider color="error">
-									Logout
-								</Dropdown.Item>
-							</Dropdown.Menu>
-						</Dropdown>
-					</Navbar.Item>
-				)}
+				{user ? <UserDropdown /> : null}
 				<Navbar.Toggle />
 			</Navbar.Content>
 		</Navbar>
